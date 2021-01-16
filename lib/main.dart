7d61a9631436fcb7d6964834b0f9222ab5530310
Jack Sari <janasarii@gmail.com>
2021-01-15 23:10:35 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+//Nosotros escribimos
+//import './src/app.dart';
+import 'package:contador/src/app.dart';
+
+void main(){
+  runApp(new MyApp());
+
+}
+
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,37 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+
+  final estiloTexto = new TextStyle(fontSize: 30);
+
+  final conteo = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Título'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de clicks: ', style: estiloTexto ),
+            Text('$conteo', style: estiloTexto)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          //print('Hola botón');
+          //conteo = conteo + 1;
+        },
+      ),
+    );
+  }
+}
